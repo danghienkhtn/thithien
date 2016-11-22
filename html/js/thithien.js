5b597c737a9b18f8115322f6be7ce1864a857ee4
Dang Hien <danghienkhtn@gmail.com>
2016-11-22 20:20:20 +0700

    22/11/2016 cty

--- html/js/thithien.js
+++ html/js/thithien.js
@@ -2,4 +2,23 @@ $(document).ready(function(){
     $("#closeBannerTop").click(function(){
         $("#bannerTop").hide();
     });
+
+    
+
+    /*$("#colMenu").mouseenter(function(){
+    	var wcolmenu = $("#colMenu").width();
+    	var wcolcontent = $("#colContent").width();
+    	console.log($("#colContent").width());
+        $("#colMenu").width(wcolmenu + 40);
+        $("#colContent").width(wcolcontent - 40);
+        console.log($("#colContent").width());
+    });
+    $("#colMenu").mouseleave(function(){
+    	var wcolmenu = $("#colMenu").width();
+    	var wcolcontent = $("#colContent").width();
+    	console.log($("#colContent").width());
+        $("#colMenu").width(wcolmenu - 40);
+        $("#colContent").width(wcolcontent + 40);
+        console.log($("#colContent").width());
+    });*/
 });
